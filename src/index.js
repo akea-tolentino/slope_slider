ca5796ef2b66bc04c6351660250b7ef805d62a71
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-05-26 09:12:05 -0700

    updated canvas width/height

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import Rider from "./rider";
 
 document.addEventListener("DOMContentLoaded", () => {
     const canvasEl = document.getElementById("game-canvas");
-    canvasEl.width = 1400;
-    canvasEl.height = 1400;
+    canvasEl.width = 1500;
+    canvasEl.height = 1500;
 
     const ctx = canvasEl.getContext("2d");
     ctx.fillStyle = "whitesmoke";
-    ctx.fillRect(0, 0, 1400, 1400);
+    ctx.fillRect(0, 0, 1500, 1500);
     
     window.ctx = ctx;
     window.MovingObject = MovingObject;

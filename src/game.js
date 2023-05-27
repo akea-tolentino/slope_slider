bf6c68b77f1fdac431592562f3e2c36970a9256f
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-05-27 11:29:17 -0700

    created a functioning event listener for play button

--- src/game.js
+++ src/game.js
@@ -1,59 +1,60 @@
-import MovingObject from "./moving_object";
-import Rider from "./rider";
-import Obstacle from "./obstacle";
-import Tree from "./tree";
-import SmallTree from "./small_tree";
+// import MovingObject from "./moving_object";
+// import Rider from "./rider";
+// import Obstacle from "./obstacle";
+// import Tree from "./tree";
+// import SmallTree from "./small_tree";
 
-class Game {
+// class Game {
 
-    static CANVAS = canvasEl;
+//     constructor(canvasEl) {
+//         this.canvas = canvasEl;
+//         this.trees = [];
+//         this.smallTrees = [];
+//     }
 
-    constructor(options) {
-        const {canvas} = options;
-        this.canvas = Game.CANVAS;
-    }
-
-    runGame() {
-
-            const trees = []
-            for (let i = 450; i < canvasEl.width; i+=600) {
-                for (let j = 0; j <= canvasEl.height; j+=300) {
-                    trees.push(
-                        new Tree({pos:[i, j]})
-                    )}}
-            trees.forEach((tree) => tree.drawTree(ctx));
+//     runGame() {
+//             for (let i = 450; i < this.canvas.width; i+=600) {
+//                 for (let j = 0; j <= this.canvas.height; j+=300) {
+//                     this.trees.push(
+//                         new Tree({pos:[i, j]})
+//                     )}}
+//             trees.forEach((tree) => tree.drawTree(ctx));
             
-            const smallTrees = []
-                for (let j = 0; j <= canvasEl.height; j+=300) {
-                    smallTrees.push(
-                        new SmallTree({pos:[0, j]})
-                    )}
-                for (let j = 0; j <= canvasEl.height; j+=300) {
-                    smallTrees.push(
-                        new SmallTree({pos:[1500, j]})
-                    )}
-            smallTrees.forEach((tree) => tree.drawTree(ctx));
+//                 for (let j = 0; j <= this.canvas.height; j+=300) {
+//                     this.smallTrees.push(
+//                         new SmallTree({pos:[0, j]})
+//                     )}
+//                 for (let j = 0; j <= this.canvas.height; j+=300) {
+//                     this.smallTrees.push(
+//                         new SmallTree({pos:[1500, j]})
+//                     )}
+//             smallTrees.forEach((tree) => tree.drawTree(ctx));
             
-            window.MovingObject = MovingObject;
-            window.Rider = Rider;
-            window.Obstacle = Obstacle;
-            window.Tree = Tree;
-            window.SmallTree = SmallTree;
-            window.player = new Rider({});
-    }
-}
+//             window.MovingObject = MovingObject;
+//             window.Rider = Rider;
+//             window.Obstacle = Obstacle;
+//             window.Tree = Tree;
+//             window.SmallTree = SmallTree;
+//             window.player = new Rider({});
+//     }
+    
+// }
 
-document.getElementById("run-game").addEventListener("click", this.runGame.bind(this));
+// var tmp = new Game(); 
+// document.getElementById("run-game").addEventListener("click", function(){
+//     tmp.runGame()}, false);
+
+// document.getElementbyId("run-game").addEventListener("click", this.runGame().bind(this));
 
 
-window.addEventListener("keydown", (e) => {
+// window.addEventListener("keydown", (e) => {
 
-    if (e.key === "a") player.moveLeft();
-    if (e.key === "d") player.moveRight();
-    if (e.key === " ") player.jump();
-});
+//     if (e.key === "a") player.moveLeft();
+//     if (e.key === "d") player.moveRight();
+//     if (e.key === " ") player.jump();
+// });
 
-export default Game;
+// export default Game;
 
 //add button with an addEventListener, which would invoke setupGame
 //button will have a id, that "click" refers to
--- src/index.js
+++ src/index.js
@@ -5,17 +5,71 @@ import Tree from "./tree";
 import SmallTree from "./small_tree";
 
 //when we load the page, canvas element is created and printed
+const canvasEl = document.getElementById("game-canvas");
 document.addEventListener("DOMContentLoaded", () => {
-    const canvasEl = document.getElementById("game-canvas");
     canvasEl.width = 1500;
     canvasEl.height = 1500;
 
     const ctx = canvasEl.getContext("2d");
     ctx.fillStyle = "whitesmoke";
     ctx.fillRect(0, 0, 1500, 1500);
+
+    // const trees = [];
+    // for (let i = 450; i < canvasEl.width; i+=600) {
+    //     for (let j = 0; j <= canvasEl.height; j+=300) {
+    //         trees.push(
+    //             new Tree({pos:[i, j]})
+    //         )}}
+    // trees.forEach((tree) => tree.drawTree(ctx));
+    
+    // const smallTrees = [];
+    //     for (let j = 0; j <= canvasEl.height; j+=300) {
+    //         smallTrees.push(
+    //             new SmallTree({pos:[0, j]})
+    //         )}
+    //     for (let j = 0; j <= canvasEl.height; j+=300) {
+    //         smallTrees.push(
+    //             new SmallTree({pos:[1500, j]})
+    //         )}
+    // smallTrees.forEach((tree) => tree.drawTree(ctx));
+    
+    // window.MovingObject = MovingObject;
+    // window.Rider = Rider;
+    // window.Obstacle = Obstacle;
+    // window.Tree = Tree;
+    // window.SmallTree = SmallTree;
+    // window.player = new Rider({});
     
     window.ctx = ctx;
 });
+
+document.getElementById("run-game").addEventListener("click", ()=> {
+    const trees = [];
+    for (let i = 450; i < canvasEl.width; i+=600) {
+        for (let j = 0; j <= canvasEl.height; j+=300) {
+            trees.push(
+                new Tree({pos:[i, j]})
+            )}}
+    trees.forEach((tree) => tree.drawTree(ctx));
+    
+    const smallTrees = [];
+        for (let j = 0; j <= canvasEl.height; j+=300) {
+            smallTrees.push(
+                new SmallTree({pos:[0, j]})
+            )}
+        for (let j = 0; j <= canvasEl.height; j+=300) {
+            smallTrees.push(
+                new SmallTree({pos:[1500, j]})
+            )}
+    smallTrees.forEach((tree) => tree.drawTree(ctx));
+    
+    window.MovingObject = MovingObject;
+    window.Rider = Rider;
+    window.Obstacle = Obstacle;
+    window.Tree = Tree;
+    window.SmallTree = SmallTree;
+    window.player = new Rider({});
+})
 
 window.addEventListener("keydown", (e) => {
 
